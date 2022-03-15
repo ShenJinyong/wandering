ab52fb171f23a04fdb6a38c553116b8adee1c867
ShenJinyong <438217638@qq.com>
2022-03-15 23:37:39 +0800

    【优化】前台主页页面布局

--- wandering-browser/src/main.js
+++ wandering-browser/src/main.js
@@ -1,5 +1,10 @@
 // 入口文件
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus,{ size: 'small', zIndex: 3000 })
+app.mount('#app')
--- wandering-browser/vue.config.js
+++ wandering-browser/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 处理eslint校验规则
+  lintOnSave: false
 })
